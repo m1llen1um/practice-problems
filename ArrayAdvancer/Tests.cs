78c692a00446cfcf44567f4d72e70fb09df3816b
Milen <m1llen1um@users.noreply.github.com>
2019-06-19 17:26:53 +0300

    New practice task

---
+++ ArrayAdvancer/Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ArrayAdvancer
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(Solution.IsTraversal());
+        }
+    }
+}
