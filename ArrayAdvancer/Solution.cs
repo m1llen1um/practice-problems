36efa72d94f79610f1fe994abfe066420880a168
Milen <m1llen1um@users.noreply.github.com>
2019-06-21 01:07:04 +0300

    overlap and buggy array advancer

--- ArrayAdvancer/Solution.cs
+++ ArrayAdvancer/Solution.cs
@@ -1,14 +1,12 @@
-/*
-You are given an array of nonnegative integers. Let's say you start at the
-beginning of the array and are trying to advance to the end. You can advance at
-most, the number of steps that you're currently on. Determine whether you can
-get to the end of the array.
-
-For example, given the array [1, 3, 1, 2, 0, 1], we can go from indices 0 -> 1
--> 3 -> 5, so return true.
-
-Given the array [1, 2, 1, 0, 0], we can't reach the end, so return false.
- */
+/// You are given an array of nonnegative integers. Let's say you start at the
+/// beginning of the array and are trying to advance to the end. You can advance at
+/// most, the number of steps that you're currently on. Determine whether you can
+/// get to the end of the array.
+///
+/// For example, given the array [1, 3, 1, 2, 0, 1], we can go from indices 0 -> 1
+/// -> 3 -> 5, so return true.
+///
+/// Given the array [1, 2, 1, 0, 0], we can't reach the end, so return false.
 
 using System;
 using Xunit;
@@ -17,18 +15,39 @@ namespace ArrayAdvancer
 {
     public static class Solution
     {
-        public static bool IsTraversal()
+        public static bool IsTraversal(int[] sourceArray)
         {
-            return true;
+            if(sourceArray.Length == 0)
+            {
+                return false;
+            }
+
+            return Traverce(0, sourceArray);
         }
 
-        // [1, 3, 1, 2, 0, 1]
-        //     *
-        //        *  *  *
-        //           *  *
-        //              *  *
-        public static bool Traverce(int nextStep, int[] sourceArray)
+        public static bool Traverce(int currentIndex, int[] sourceArray)
         {
+            int nextStep = sourceArray[currentIndex];
+            if (nextStep == 0)
+            {
+                return false;
+            }
+
+            var nextIndex = currentIndex + nextStep;
+            if (nextIndex >= sourceArray.Length - 1)
+            {
+                return nextIndex == sourceArray.Length - 1;
+            }
+
+            bool pathNotFound = false;
+            int nextStepIndex = nextStep;
+            while (!pathNotFound && nextStepIndex > 0)
+            {
+                pathNotFound = Traverce(currentIndex + nextStepIndex, sourceArray);
+                nextStepIndex--;
+            }
+
+            return pathNotFound;
         }
     }
 }
--- ArrayAdvancer/Tests.cs
+++ ArrayAdvancer/Tests.cs
@@ -6,9 +6,21 @@ namespace ArrayAdvancer
     public class Tests
     {
         [Fact]
-        public void Test()
+        public void GoodPathTest()
         {
-            Assert.True(Solution.IsTraversal());
+            Assert.True(Solution.IsTraversal(new int[] { 1, 3, 1, 2, 0, 1 }));
+        }
+
+        [Fact]
+        public void BadPathTest()
+        {
+            Assert.False(Solution.IsTraversal(new int[] { 1, 2, 1, 0, 0 }));
+        }
+
+        [Fact]
+        public void NullOrEmptyTest()
+        {
+            Assert.False(Solution.IsTraversal(new int[] { }));
         }
     }
 }
